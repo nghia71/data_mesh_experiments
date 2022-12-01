6d379ec4969d3cdf2612925a874a98d09a58ff0a
Nghia Doan <nghia.dh@mac.com>
2022-12-01 14:39:21 -0500

    Not overwrite gpg

--- install_docker_engine.sh
+++ install_docker_engine.sh
@@ -16,8 +16,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-exit 0
-
 # Install Docker Engine
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
