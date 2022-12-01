c1e46dd06f7572662828faad79931b2e7afb77e7
Nghia Doan <nghia.dh@mac.com>
2022-12-01 14:38:38 -0500

    Not overwrite gpg

--- install_docker_engine.sh
+++ install_docker_engine.sh
@@ -11,10 +11,12 @@ sudo apt-get install \
     gnupg \
     lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL -C - https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL -C - https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+exit 0
 
 # Install Docker Engine
 sudo apt-get update -y
