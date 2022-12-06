c97a8024d6ca5c2c60c872d97c2307b9987f219f
Nghia Doan <nghia.dh@mac.com>
2022-12-06 14:54:11 -0500

    Kafka 2B.

--- 1st_us_covid/set_kafka_stogare_uuid.sh
+++ 1st_us_covid/set_kafka_stogare_uuid.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-KAFKA_STORAGE_UUID=$(cat /proc/sys/kernel/random/uuid | tr -d '-' | base64 | cut -b 1-22)
+KAFKA_STORAGE_UUID="$(cat /proc/sys/kernel/random/uuid | tr -d '-' | base64 | cut -b 1-22)"
+sed -i 's/KAFKA_STORAGE_UUID=/KAFKA_STORAGE_UUID=${KAFKA_STORAGE_UUID}/' .env
+echo 
