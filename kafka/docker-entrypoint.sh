debea6c976355b3917a4531092ba419331c97ec4
Nghia Doan <nghia.dh@mac.com>
2022-12-06 00:59:13 -0500

    Kafka K1B.

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -8,15 +8,19 @@ _term() {
 trap _term SIGINT SIGTERM
 
 properties_file=/opt/kafka/config/kraft/server.properties;
-kafka_addr=localhost:9092
+kafka_addr=$KRAFT_HOST:$KRAF_BPORT
+
+echo $kafka_addr
 
 echo "Applying environment variables ...";
+echo "controller.quorum.voters=${KRAFT_ID}@${KRAFT_HOST}:${KRAF_CPORT}"
+echo "controller.quorum.voters=${KRAFT_ID}@${KRAFT_HOST}:${KRAF_CPORT}" >> $properties_file;
 echo "inter.broker.listener.name=PLAINTEXT" >> $properties_file;
 echo "controller.listener.names=CONTROLLER" >> $properties_file;
-echo "listeners=PLAINTEXT://:9092,CONTROLLER://:19092" >> $properties_file;
+echo "listeners=PLAINTEXT://:${KRAF_BPORT},CONTROLLER://:${KRAF_CPORT}"
+echo "listeners=PLAINTEXT://:${KRAF_BPORT},CONTROLLER://:${KRAF_CPORT}" >> $properties_file;
 echo "listener.security.protocol.map=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL" >> $properties_file;
 echo "Enivronment variables applied ✅";
-
 
 echo "Setting up Kafka storage ...";
 export suuid=$(./bin/kafka-storage.sh random-uuid);
