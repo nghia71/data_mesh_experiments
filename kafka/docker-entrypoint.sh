151d6f47d5a376bd59cd2ac22f54102f418557f0
Nghia Doan <nghia.dh@mac.com>
2022-12-06 01:05:55 -0500

    Kafka K1B.

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -13,10 +13,8 @@ kafka_addr=$KRAFT_HOST:$KRAFT_BPORT
 echo $kafka_addr
 
 echo "Applying environment variables ...";
-# echo "controller.quorum.voters=${KRAFT_ID}@${KRAFT_HOST}:${KRAFT_CPORT}"
 echo "process.roles=broker,controller" >> $properties_file;
-echo "process.roles=broker,controller" >> $properties_file;
-echo "node.id=${KRAFT_ID}$ >> $properties_file;
+echo "node.id=${KRAFT_ID}$" >> $properties_file;
 echo "controller.quorum.voters=${KRAFT_ID}@${KRAFT_HOST}:${KRAFT_CPORT}" >> $properties_file;
 echo "inter.broker.listener.name=PLAINTEXT" >> $properties_file;
 echo "controller.listener.names=CONTROLLER" >> $properties_file;
