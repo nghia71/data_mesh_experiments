ce3b75db0274d303cf4e84862c7327feebb94abc
Nghia Doan <nghia.dh@mac.com>
2022-12-06 00:11:40 -0500

    Kafka K1B.

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -14,7 +14,8 @@ echo "Applying environment variables ...";
 echo "advertised.listeners=CONTROLLER://${KRAFT_CONTAINER_HOST_NAME}:19092,LOCALHOST://localhost:29092,OUTSIDE://0.0.0.0:9093" >> $properties_file;
 echo "listeners=CONTROLLER://0.0.0.0:19092,LOCALHOST://localhost:29092,OUTSIDE://0.0.0.0:9093" >> $properties_file;
 echo "listener.security.protocol.map=CONTROLLER:PLAINTEXT,LOCALHOST:PLAINTEXT,OUTSIDE:PLAINTEXT" >> $properties_file;
-echo "inter.broker.listener.name=PLAINTEXT" >> $properties_file;
+echo "controller.listener.names=CONTROLLER" >> $properties_file;
+echo "inter.broker.listener.name=CONTROLLER" >> $properties_file;
 echo "Enivronment variables applied ✅";
 
 
