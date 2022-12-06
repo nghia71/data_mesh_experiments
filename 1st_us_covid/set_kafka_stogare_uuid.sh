e259396f8b4a113174a4011547cc135d7da46bdd
Nghia Doan <nghia.dh@mac.com>
2022-12-06 14:57:15 -0500

    1st_us_covid.

--- 1st_us_covid/set_kafka_stogare_uuid.sh
+++ 1st_us_covid/set_kafka_stogare_uuid.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 KAFKA_STORAGE_UUID="$(cat /proc/sys/kernel/random/uuid | tr -d '-' | base64 | cut -b 1-22)"
-sed -i 's/KAFKA_STORAGE_UUID=/KAFKA_STORAGE_UUID=${KAFKA_STORAGE_UUID}/' .env
-echo 
+sed -i 's/KAFKA_STORAGE_UUID=/KAFKA_STORAGE_UUID='${KAFKA_STORAGE_UUID}'/' .env
+cat .env
