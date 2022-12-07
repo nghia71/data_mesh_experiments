6db0d4144c53ca333afd35d48f4f0e2db52e7196
Nghia Doan <nghia.dh@mac.com>
2022-12-07 15:43:21 -0500

    Kafka Test.

--- setup.sh
+++ setup.sh
@@ -6,11 +6,10 @@ sed 's/KAFKA_STORAGE_UUID=.*/KAFKA_STORAGE_UUID='${KAFKA_STORAGE_UUID}'/' .kafka
 echo "Kafka storage UUID is set to ${KAFKA_STORAGE_UUID} ✅";
 
 echo "Create volumes for data and logs ...";
-for i in {1..3}
-do
-    mkdir -p ./vol${i}/kafka-data
-    mkdir -p ./vol${i}/kafka-logs
-    chown -R $(id -u):$(id -g) ./vol${i}/kafka-data
-    chown -R $(id -u):$(id -g) ./vol${i}/kafka-logs
+for i in {1..3}; do
+    mkdir -p ./vol${i}/kafka-data;
+    mkdir -p ./vol${i}/kafka-logs;
+    chown -R $(id -u):$(id -g) ./vol${i}/kafka-data;
+    chown -R $(id -u):$(id -g) ./vol${i}/kafka-logs;
 done
 echo "Volumes for Kafka data and logs are created ✅";
