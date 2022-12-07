87557973391c514c215df4735f2e709117ba08f8
Nghia Doan <nghia.dh@mac.com>
2022-12-07 15:18:18 -0500

    Kafka Test.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ CURRENT_DIR="$(pwd)"
 export $(grep -v '^#' .env | xargs)
 
 KAFKA_IMAGE_NAME="${DOCKERHUB_ACCOUNT}/${KAFKA_IMAGE}:${KAFKA_IMAGE_VERSION}"
-HAS_IMAGE="$(docker image inspect ${KAFKA_IMAGE_NAME})"
+HAS_IMAGE="$(docker images | grep ${KAFKA_IMAGE_NAME})"
 if [ -z $HAS_IMAGE ]; then
     cd kafka
     docker build --pull -t ${KAFKA_IMAGE_NAME} .
