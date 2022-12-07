9da7a8a92e273da4fc1dc469d2dfc6a396e4fc2e
Nghia Doan <nghia.dh@mac.com>
2022-12-07 14:26:22 -0500

    Kafka Test.

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+KAFKA_IMAGE="$(docker compose images | grep kafka)"
+if [ -z $KAFKA_IMAGE ]; then
+    docker compose build --pull nghiadh/kafka-kraft:latest
+    echo "No topic requested for creation ✅";
+fi
+
 docker compose up -d
