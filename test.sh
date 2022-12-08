899487069159d2062b40dcdbfddcc3a689183dcf
Nghia Doan <nghia.dh@mac.com>
2022-12-08 15:34:20 -0500

    Kafka setup.

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 source .env
 
 echo "Create ${KRAFT_TEST_TOPIC} ...";
-docker exec --user $(id -u):$(id -g) ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-topics.sh \
+docker exec ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-topics.sh \
     --create --topic ${KRAFT_TEST_TOPIC} \
     --partitions ${KRAFT_TEST_PARTITIONS} \
     --replication-factor ${KRAFT_TEST_REPLICATION_FACTOR} \
@@ -15,13 +15,13 @@ echo $'12345\nabcde\n@$#^%$' > sent_messages.txt
 NO_MESSAGES="$(cat sent_messages.txt | wc -l)"
 
 echo "Sending ${NO_MESSAGES} messages into ${KRAFT_TEST_TOPIC} ...";
-docker exec -user $(id -u):$(id -g) --interactive --tty ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-console-producer.sh \
+docker exec --interactive --tty ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-console-producer.sh \
     --topic ${KRAFT_TEST_TOPIC} \
     --bootstrap-server ${LOCAL_IP}:${KRAFT_1_EXTERNAL_PORT} < sent_messages.txt;
 echo "${NO_MESSAGES} messages sent ✅";
 
 echo "Receiving ${NO_MESSAGES} messages from ${KRAFT_TEST_TOPIC} ...";
-docker exec -user $(id -u):$(id -g) --interactive --tty ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-console-consumer.sh \
+docker exec --interactive --tty ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-console-consumer.sh \
     --topic ${KRAFT_TEST_TOPIC} \
     --from-beginning --max-messages ${NO_MESSAGES} \
     --bootstrap-server ${LOCAL_IP}:${KRAFT_1_EXTERNAL_PORT} > recv_messages.txt;
@@ -34,7 +34,7 @@ if [ -z "$(diff sent_messages.txt recv_messages.txt)" ]; then
     rm sent_messages.txt recv_messages.txt
 
     echo "Deleting ${KRAFT_TEST_TOPIC} ...";
-    docker exec --user $(id -u):$(id -g) ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-topics.sh \
+    docker exec ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-topics.sh \
         --delete --topic ${KRAFT_TEST_TOPIC} \
         --bootstrap-server ${LOCAL_IP}:${KRAFT_1_EXTERNAL_PORT};
     echo "${KRAFT_TEST_TOPIC} deleted ✅";
