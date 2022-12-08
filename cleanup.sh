18ec73b1cadbf1cb9dfcd5d6b039062f457d0b77
Nghia Doan <nghia.dh@mac.com>
2022-12-08 14:59:17 -0500

    Kafka setup.

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,9 @@
 
 ./stop.sh
 
+sed -i 's/LOCAL_IP=/LOCAL_IP='${LOCAL_IP}'/' .env;
+sed -i 's/KAFKA_STORAGE_UUID=/KAFKA_STORAGE_UUID='${KAFKA_STORAGE_UUID}'/' .env;
+
 echo "Pruning docker system ...";
 docker system prune -a -f
 echo "Docker system pruned ✅";
