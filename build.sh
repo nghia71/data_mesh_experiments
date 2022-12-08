ca0b6a991ffb50f87beca0f22ecc3aa4204b35af
Nghia Doan <nghia.dh@mac.com>
2022-12-08 11:48:05 -0500

    Kafka setup.

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@ source .docker_env;
 KAFKA_IMAGE="${DOCKERHUB_ACCOUNT}/${KAFKA_IMAGE_NAME}";
 KAFKA_TAGGED_IMAGE="${KAFKA_IMAGE}:${KAFKA_IMAGE_VERSION}";
 if [ -z "$(docker images | grep ${KAFKA_IMAGE})" ]; then
-    echo 'no images'
     cd kafka;
-    docker build --user $(id -u):$(id -g) -t ${KAFKA_TAGGED_IMAGE} .;
+    docker build -t ${KAFKA_TAGGED_IMAGE} .;
     cd ..;
 fi
 
