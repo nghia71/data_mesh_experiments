aa5ab5bd4612711da56f575af3edbac932297fcf
Nghia Doan <nghia.dh@mac.com>
2022-12-08 17:10:31 -0500

    Kafka 3.3.1 containerized.

--- test.sh
+++ test.sh
@@ -10,7 +10,8 @@ docker exec ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-topics.sh \
     --bootstrap-server ${LOCAL_IP}:${KRAFT_1_EXTERNAL_PORT};
 echo "${KRAFT_TEST_TOPIC} created ✅";
 
-NO_MESSAGES=3
+NO_MESSAGES=3;
+seq ${NO_MESSAGES} > sent.txt;
 
 echo "Sending ${NO_MESSAGES} messages into ${KRAFT_TEST_TOPIC} ...";
 docker exec ${KRAFT_1_CONTAINER_NAME} \
@@ -21,5 +22,19 @@ echo "Receiving ${NO_MESSAGES} messages from ${KRAFT_TEST_TOPIC} ...";
 docker exec ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-console-consumer.sh \
     --topic ${KRAFT_TEST_TOPIC} \
     --from-beginning --max-messages ${NO_MESSAGES} \
-    --bootstrap-server ${LOCAL_IP}:${KRAFT_1_EXTERNAL_PORT}
+    --bootstrap-server ${LOCAL_IP}:${KRAFT_1_EXTERNAL_PORT} > recv.txt
 echo "${NO_MESSAGES} messages received ✅";
+
+if [ -z "$(diff recv.txt sent.txt)" ]; then
+    echo 'Sent and receive messages matched  ✅'
+else
+    echo 'ERROR: Sent and receive messages did not match.'
+fi
+
+rm recv.txt sent.txt
+
+echo "Deleting ${KRAFT_TEST_TOPIC} ...";
+docker exec ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-topics.sh \
+    --delete --topic ${KRAFT_TEST_TOPIC} \
+    --bootstrap-server ${LOCAL_IP}:${KRAFT_1_EXTERNAL_PORT};
+echo "${KRAFT_TEST_TOPIC} deleted ✅";
