cbdf442a1792acff355e2b920c9358ed29bf3cb9
Nghia Doan <nghia.dh@mac.com>
2022-12-09 13:35:54 -0500

    Test multihost.

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ docker exec ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-topics.sh \
     --create --topic ${KRAFT_TEST_TOPIC} \
     --partitions ${KRAFT_TEST_PARTITIONS} \
     --replication-factor ${KRAFT_TEST_REPLICATION_FACTOR} \
-    --bootstrap-server ${LOCAL_IP}:${KRAFT_1_EXTERNAL_PORT};
+    --bootstrap-server ${KRAFT_1_CONTAINER_NAME}:${KRAFT_1_EXTERNAL_PORT};
 echo "${KRAFT_TEST_TOPIC} created ✅";
 
 NO_MESSAGES=3;
@@ -15,14 +15,14 @@ seq ${NO_MESSAGES} > sent.txt;
 
 echo "Sending ${NO_MESSAGES} messages into ${KRAFT_TEST_TOPIC} ...";
 docker exec ${KRAFT_1_CONTAINER_NAME} \
-    bash -c "seq ${NO_MESSAGES} | ./bin/kafka-console-producer.sh  --topic ${KRAFT_TEST_TOPIC}  --bootstrap-server ${LOCAL_IP}:${KRAFT_1_EXTERNAL_PORT}"
+    bash -c "seq ${NO_MESSAGES} | ./bin/kafka-console-producer.sh  --topic ${KRAFT_TEST_TOPIC}  --bootstrap-server ${KRAFT_2_CONTAINER_NAME}:${KRAFT_2_EXTERNAL_PORT}"
 echo "${NO_MESSAGES} messages sent ✅";
 
 echo "Receiving ${NO_MESSAGES} messages from ${KRAFT_TEST_TOPIC} ...";
 docker exec ${KRAFT_1_CONTAINER_NAME} ./bin/kafka-console-consumer.sh \
     --topic ${KRAFT_TEST_TOPIC} \
     --from-beginning --max-messages ${NO_MESSAGES} \
-    --bootstrap-server ${LOCAL_IP}:${KRAFT_1_EXTERNAL_PORT} > recv.txt
+    --bootstrap-server ${LOCAL_IP}:${KRAFT_2_EXTERNAL_PORT} > recv.txt
 echo "${NO_MESSAGES} messages received ✅";
 
 if [ -z "$(diff recv.txt sent.txt)" ]; then
